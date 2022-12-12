13b635214f4ab9971f4cdec64a4248fc77758e4f
jungtaek kim <kgajungtaek@gmail.com>
2022-12-12 17:01:33 +0900

    feat : Navigation 이동
    게임월드의 내비게이션 공간을 지정한 뒤 게임오브젝트 내비게이션 요원에게 목적지를 설정하는 방식의 이동
    직접 조작하여 이동하는 것이 아닌 목적지만이 중요한 경우 적합
    ex. RTS, 몬스터이동

---
+++ Assets/Scripts/NavigationMover.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Apple;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class NavigationMover : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+	[SerializeField]
+	private Vector3 destination;
+
+	private void Awake()
+	{
+		agent = GetComponent<NavMeshAgent>();
+	}
+
+	private void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			// Camera.main : 태그가 "Camera"인 오브젝트 = "Main Camera"
+			// 카메라로부터 마우스 좌표(Input.mousePosition) 위치를 관통하는 광선 생성
+			// Ray ray = new Ray(시작점, 방향);
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit))
+			{
+				destination = hit.point;
+			}
+		}
+
+		agent.destination = destination;
+	}
+}
